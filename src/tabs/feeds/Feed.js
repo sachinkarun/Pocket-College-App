b8e9f7463203527a8e599748fcf4023c6cf1c36c
Sachin <naugthyshinchan@gmail.com>
2023-01-14 18:34:29 +0530

    firebase integration

--- src/tabs/feeds/Feed.js
+++ src/tabs/feeds/Feed.js
@@ -3,12 +3,12 @@ import { View, Text, TextInput, Button } from 'react-native';
 import { useSelector, useDispatch } from 'react-redux';
 import { changeName } from '../../redux/action';
 
+
 const Feed = () => {
     const [name, setName] = useState("");
     const user = useSelector(state => state.user.currentUser);
-
     const dispatch = useDispatch();
-
+    
     const handleSubmit = () => {
         if(name.length === 0) return;
 
